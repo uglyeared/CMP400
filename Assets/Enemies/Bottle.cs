29e438ca06a99c03df52a397228f3e30ed4e3801
Parker <mikasa9ve@gmail.com>
2026-02-13 03:58:39 +0000

    fixed
    i misunderstood how formatting in c# works vs c++, simple error to fix. bottle.cs is obsolete but keeping around just in case.

--- Assets/Enemies/Bottle.cs
+++ Assets/Enemies/Bottle.cs
@@ -1,7 +1,49 @@
+using UnityEngine.Events;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Bottle", menuName = "Scriptable Objects/Bottle")]
+
 public class Bottle : ScriptableObject
 {
-    
+
+    public int WeaponType; //to be changed by use of ui
+    public bool WeaponSide;
+
+
+
+    void onThrow()
+    {
+
+
+        switch (WeaponType)
+        {
+            case 1:
+                bottleType1();
+                break;
+                case 2:
+                bottleType2();
+                break;
+                case 3:
+                bottleType3();
+                break;
+        }
+      
+
+    }
+
+
+    void bottleType1()
+    {
+        WeaponSide = true;
+    }
+    void bottleType2()
+    {
+        WeaponSide = true;
+
+    }
+    void bottleType3()
+    {
+        WeaponSide = false;
+
+    }
 }
--- Assets/Scripts/ThrowableWeapon.cs
+++ Assets/Scripts/ThrowableWeapon.cs
@@ -2,15 +2,63 @@ using UnityEngine;
 
 public class ThrowableWeapon : MonoBehaviour
 {
+
+    public int WeaponType; //to be changed by use of ui
+    public bool WeaponSide;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
+    { }
+   
+    
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    void onSliderSwitch()
+    {
+
+
+        switch (WeaponType)
+        {
+            case 1:
+                bottleType1();
+                break;
+            case 2:
+                bottleType2();
+                break;
+            case 3:
+                bottleType3();
+                break;
+        }
+
+
+    }
+
+
+    void bottleType1()
+    {
+        WeaponSide = true;
+    }
+    void bottleType2()
+    {
+        WeaponSide = true;
+
+    }
+    void bottleType3()
+    {
+        WeaponSide = false;
+
+    }
+    
 }
+
+
+
+
+
+
+  
